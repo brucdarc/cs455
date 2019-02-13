0f1b7b0cd23a1e14dd4442b20ee24c1956f7ebec
brucdarc <cutreaper@gmail.com>
2019-02-13 12:39:27 -0700

    cleaned up bash script

--- Project1/startup.sh
+++ Project1/startup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 machineList=$1
 numNodes=$2
-registryPort=$3
-echo Number of Nodes $numNodes
-echo Registry Port $registryPort
+registryIp=$3
+registryPort=$4
+echo Required Arguments
+echo Machine List: $machineList
+echo Number of Nodes: $numNodes
+echo Registry Ip: $registryIp
+echo Registry Port: $registryPort
+echo The registry must be the machine you are on.
 
 machines=()
 
@@ -11,16 +16,16 @@ for F in $(cat $machineList) ; do
   machines+=(${F})
 done
 
-echo ${machines[0]}
+#echo ${machines[0]}
 
 
-gnome-terminal -- bash -c "ls; java -cp build/classes/java/main/ cs455.overlay.node.Registry $registryPort;"
+gnome-terminal -- bash -c "java -cp build/classes/java/main/ cs455.overlay.node.Registry $registryPort;"
 
 for ((i=1; i<=$numNodes; i++))
 do 
 	
-	gnome-terminal --tab -t Messenger_Node -- bash -c "ssh ${machines[i]} 'sleep 2; java -cp Desktop/cs455/Project1/build/classes/java/main/ cs455.overlay.node.MessagingNode denver.cs.colostate.edu $registryPort'"
-	echo $i
+	gnome-terminal --tab -t Messenger_Node -- bash -c "ssh ${machines[i]} 'sleep 2; java -cp Desktop/cs455/Project1/build/classes/java/main/ cs455.overlay.node.MessagingNode $registryIp $registryPort'"
+	echo Started Node: $i
 done
 
 
